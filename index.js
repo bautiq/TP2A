5838c137f6df71f445b95302abe4c3e6a4c8d45d
bauti <bautiquere@gmail.com>
2020-05-13 17:55:22 -0300

    desafio 2 bautista querejeta

--- index.js
+++ index.js
@@ -1,7 +1,111 @@
-const validator = require('validator');
-
-console.log(validator.isEmail('pablof@tecnoshare.com'));
-
 const chalk = require('chalk');
+const MongoClient = require('mongodb').MongoClient;
+const uri = 'mongodb+srv://bauti:asd123@clusterbautitp-bp5ff.mongodb.net/test?retryWrites=true&w=majority';
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
-console.log(chalk.blue.bgGray('Hello world'));
+
+    client.connect(err => {
+        if (err == null || err == undefined) {
+          console.log(chalk.green('connected and performing task'));
+
+          const database = client.db('desafio2db');
+          const collectionInventors = database.collection('inventors');
+
+          const inventor = {first: 'Leonardo', last: 'Da Vinci', year:1452};
+
+          /*
+         showInventors(collectionInventors) .then(()=> {
+            console.log(chalk.green('Finished showing inventors'));
+          })
+
+          .catch((error)=>{
+            console.log(chalk.red('Failed when showing investors' + error));
+          });*/
+
+        /*
+        insertInventor(collectionInventors, inventor).then(()=> {
+            console.log(chalk.green('Finished inserting inventor'));
+        })
+          
+          .catch((error)=>{
+            console.log(chalk.red('Failed when inserting inventor' + error));
+          });*/
+
+        /*
+        updateInventor(collectionInventors, inventor, {$set:{death: 1519}}).then(()=> {
+            console.log(chalk.green('Finished updating inventor'));
+        })
+          
+          .catch((error)=>{
+            console.log(chalk.red('Failed when updating inventor' + error));
+          });*/
+        
+
+        /*
+        deleteInventor(collectionInventors, {first: 'Leonardo', year: 1452}).then(()=> {
+             console.log(chalk.green('Finished deleting inventor'));
+        })
+        
+        .catch((error)=>{
+            console.log(chalk.red('Failed when deleting inventor' + error));
+          });
+         
+      } else {
+          console.log(chalk.red('error in connection, code:' + err.code))
+      }*/
+        
+        console.log(chalk.blue('Closing connection, bye'));
+        client.close();
+      });
+
+
+
+
+
+const showInventors = async function(collection) {
+
+    new Promise((resolve) => {
+        resolve(collection.find().toArray((err, result) => {
+            console.log(result);
+        }));
+    });
+    
+}
+
+const insertInventor = async function(collection, newInventor) {
+    new Promise((resolve) => {
+        resolve(collection.insertOne(newInventor));
+    });
+    
+}
+
+const updateInventor = async function(collection,inventor, update) {
+    new Promise((resolve) => {
+        resolve(collection.updateOne(inventor, update));
+    });
+}
+
+
+
+const deleteInventor = async function(collection, inventor) {
+
+    new Promise((resolve) => {
+        resolve(collection.deleteOne(inventor));
+    });
+    
+}
+
+async function populateInventors(){
+    const database = client.db('desafio2db');
+    const collectionInventors = database.collection('inventors');
+    collectionInventors.insertMany([
+        { first: 'Albert', last: 'Einstein', year: 1879 },
+        { first: 'Isaac', last: 'Newton', year: 1643 },
+        { first: 'Galileo', last: 'Galilei', year: 1564 },
+        { first: 'Marie', last: 'Curie', year: 1867 },
+        { first: 'Johannes', last: 'Kepler', year: 1571 },
+        { first: 'Nicolaus', last: 'Copernicus', year: 1473 },
+        { first: 'Max', last: 'Planck', year: 1858 }
+    ]);
+}
+
